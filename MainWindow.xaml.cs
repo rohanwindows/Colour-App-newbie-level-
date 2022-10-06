19252a65668f5413d7c8d26fdf67bd104d4df053
Rohan Windows <115149546+rohanwindows@users.noreply.github.com>
2022-10-06 15:30:44 +0530

    Launch "setup.exe" / "ColourProject.msi"
    Basic Windows Installer is used. Installer text contains several grammatical errors. New revised version will be uploaded to 
    github.com/rohanwindows

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ColourApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Change_color.Background = new SolidColorBrush(Colors.LavenderBlush);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Change_color.Background = new SolidColorBrush(Colors.OrangeRed);
+        }
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Change_color.Background = new SolidColorBrush(Colors.Yellow);
+        }
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Change_color.Background = new SolidColorBrush(Colors.ForestGreen);
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
